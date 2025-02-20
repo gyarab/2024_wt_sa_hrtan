25acd739cd62fd3d5102686af1578d2964d80cd8
Lukas <panda98@post.cz>
2025-02-20 09:50:01 +0100

    zevicka

---
+++ zelva.py
@@ -0,0 +1,21 @@
+﻿from math import sqrt
+from turtle import *
+
+scale = 200
+diagonal = sqrt(scale ** 2 + scale ** 2)
+forward(scale)
+left(135)
+forward(diagonal)
+left(135)
+forward(scale)
+left(135)
+forward(diagonal)
+left(90)
+forward(diagonal / 2)
+left(90)
+forward(diagonal / 2)
+left(135)
+forward(scale)
+right(90)
+forward(scale)
+exitonclick()
